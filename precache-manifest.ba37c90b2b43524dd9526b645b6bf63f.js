a3e3c4fae85538317a54e73fcd15f782dc22716e
mayankmohak <mayank8199@gmail.com>
2023-03-09 17:24:26 +0530

    Updates

---
+++ precache-manifest.ba37c90b2b43524dd9526b645b6bf63f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e69545b48c08ed22a19",
+    "url": "/roomies-web/static/css/main.1e44ecc6.chunk.css"
+  },
+  {
+    "revision": "6e69545b48c08ed22a19",
+    "url": "/roomies-web/static/js/main.6e69545b.chunk.js"
+  },
+  {
+    "revision": "de570238e6ced32f40ad",
+    "url": "/roomies-web/static/js/1.de570238.chunk.js"
+  },
+  {
+    "revision": "ccb638aa0352254e0e47",
+    "url": "/roomies-web/static/js/runtime~main.ccb638aa.js"
+  },
+  {
+    "revision": "d7fa760620c8d809b0d4807ca1602140",
+    "url": "/roomies-web/index.html"
+  }
+];
